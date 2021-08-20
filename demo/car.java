bccdf6bb709dfee3d89f1e9fa577f647c315612b
sonu parihar <sparihar729@gmail.com>
2021-08-21 00:48:50 +0530

    This is my frist commit

---
+++ demo/car.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class car {
+		
+	public static void main(String[] args) {
+		car car=new car();
+		int a=1;
+		int b=2;
+		int c=a+b;
+
+		System.out.println(car.equals(car));
+		System.out.println(c);
+	}
+
+}
